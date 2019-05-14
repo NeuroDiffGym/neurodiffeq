fdf8c18404df47c3bfa03137b070e685f1bf8d0e
Feiyu <feiyu_chen@g.harvard.edu>
2019-05-14 10:05:09 -0400

    install requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
+import os
 from setuptools import setup, find_packages
 
-setup(name='neurodiffeq', packages=find_packages())
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+        REQUIRED = f.read().split('\n')
+except:
+    REQUIRED = []
+
+setup(name='neurodiffeq', packages=find_packages(),
+      install_requires=REQUIRED)
