948c1ef41d1234403e18f2739319601990563496
Feiyu <feiyu_chen@g.harvard.edu>
2019-09-04 20:53:04 -0400

    Updated tests.

--- tests/test_ode.py
+++ tests/test_ode.py
@@ -8,7 +8,8 @@ from neurodiffeq.ode import IVP, DirichletBVP
 from neurodiffeq.ode import solve, solve_system, Monitor, ExampleGenerator
 
 from pytest import raises
-    
+
+
 def test_monitor():
     exponential = lambda x, t: diff(x, t) - x
     init_val_ex = IVP(t_0=0.0, x_0=1.0)
@@ -18,39 +19,43 @@ def test_monitor():
                            monitor=Monitor(t_min=0.0, t_max=2.0, check_every=1))
     print('Monitor test passed.')
 
-def test_example_generator():
+
+def test_train_generator():
     exponential = lambda x, t: diff(x, t) - x
     init_val_ex = IVP(t_0=0.0, x_0=1.0)
     
     train_gen = ExampleGenerator(size=32, t_min=0.0, t_max=2.0, method='uniform')
     solution_ex, _ = solve(ode=exponential, condition=init_val_ex, 
                            t_min=0.0, t_max=2.0,
-                           example_generator=train_gen,
+                           train_generator=train_gen,
                            max_epochs=3)
     train_gen = ExampleGenerator(size=32, t_min=0.0, t_max=2.0, method='equally-spaced')
     solution_ex, _ = solve(ode=exponential, condition=init_val_ex, 
                            t_min=0.0, t_max=2.0,
-                           example_generator=train_gen,
+                           train_generator=train_gen,
                            max_epochs=3)
     train_gen = ExampleGenerator(size=32, t_min=0.0, t_max=2.0, method='equally-spaced-noisy')
     solution_ex, _ = solve(ode=exponential, condition=init_val_ex, 
                            t_min=0.0, t_max=2.0,
-                           example_generator=train_gen,
+                           train_generator=train_gen,
                            max_epochs=3)
     with raises(ValueError):
         train_gen = ExampleGenerator(size=32, t_min=0.0, t_max=2.0, method='magic')
     print('ExampleGenerator test passed.')
-        
+
+
 def test_ode():
     exponential = lambda x, t: diff(x, t) - x
     init_val_ex = IVP(t_0=0.0, x_0=1.0)
-    solution_ex, _ = solve(ode=exponential, condition=init_val_ex, 
-                           t_min=0.0, t_max=2.0, shuffle=False)
+    solution_ex, _ = solve(ode=exponential, condition=init_val_ex,
+                           t_min=0.0, t_max=2.0, shuffle=False,
+                           max_epochs=2000)
     ts = np.linspace(0, 2.0, 100)
     x_net = solution_ex(ts, as_type='np')
     x_ana = np.exp(ts)
     assert isclose(x_net, x_ana, atol=0.1).all()
     print('solve basic test passed.')
+
 
 def test_ode_system():
     
@@ -73,6 +78,7 @@ def test_ode_system():
     assert isclose(x2_net, x2_ana, atol=0.1).all()
     print('solve_system basic test passed.')
 
+
 def test_ode_ivp():
     oscillator = lambda x, t: diff(x, t, order=2) + x
     init_val_ho = IVP(t_0=0.0, x_0=0.0, x_0_prime=1.0)
@@ -85,6 +91,7 @@ def test_ode_ivp():
     assert isclose(x_net, x_ana, atol=0.1).all()
     print('IVP basic test passed.')
 
+
 def test_ode_bvp():
     oscillator = lambda x, t: diff(x, t, order=2) + x
     bound_val_ho = DirichletBVP(t_0=0.0, x_0=0.0, t_1=1.5*np.pi, x_1=-1.0)
@@ -96,7 +103,8 @@ def test_ode_bvp():
     x_ana = np.sin(ts)
     assert isclose(x_net, x_ana, atol=0.1).all()
     print('BVP basic test passed.')
-    
+
+
 def test_lotka_volterra():
     alpha, beta, delta, gamma = 1, 1, 1, 1
     lotka_volterra = lambda x, y, t : [diff(x, t) - (alpha*x  - beta*x*y), 
@@ -124,8 +132,3 @@ def test_lotka_volterra():
     assert isclose(prey_net, prey_num, atol=0.1).all()
     assert isclose(pred_net, pred_num, atol=0.1).all()
     print('Lotka Volterra test passed.')
-    
-        
-    
-    
-    
