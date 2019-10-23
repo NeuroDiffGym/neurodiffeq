3c6d2e568a345ae174992665b770c9f0f1bd728e
Feiyu <feiyu_chen@g.harvard.edu>
2019-10-23 14:13:11 -0400

    Updated setup.py to include more info.

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-import os
-from setuptools import setup, find_packages
+import setuptools
 
-here = os.path.abspath(os.path.dirname(__file__))
-try:
-    with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-        REQUIRED = f.read().split('\n')
-except:
-    REQUIRED = []
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(name='neurodiffeq', packages=find_packages(),
-      install_requires=REQUIRED)
+setuptools.setup(
+    name="neurodiffeq",
+    version="0.0.1",
+    author="odegym",
+    author_email="feiyu_chen@g.harvard.edu",
+    description="A Python package for solving differential equations with neural networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/odegym/neurodiffeq",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
