f889c025c1fe8ec1a5309640a26507e34a4c13f2
Feiyu <feiyu_chen@g.harvard.edu>
2020-03-07 13:51:36 -0500

    _solve should be general train and valid should be 1-d/2-d/3-d specific

--- tests/test_temporal.py
+++ tests/test_temporal.py
@@ -7,7 +7,7 @@ from neurodiffeq.temporal import generator_1dspatial, generator_temporal
 from neurodiffeq.temporal import FirstOrderInitialCondition, BoundaryCondition
 from neurodiffeq.temporal import SingleNetworkApproximator1DSpatialTemporal
 from neurodiffeq.temporal import Monitor1DSpatialTemporal
-from neurodiffeq.temporal import _train, _valid
+from neurodiffeq.temporal import _train_1dspatial_temporal, _valid_1dspatial_temporal
 import matplotlib
 matplotlib.use('Agg') # use a non-GUI backend, so plots are not shown during testing
 
@@ -188,7 +188,7 @@ def test_monitor_1dspatial_temporal():
     )
     monitor.check(fcnn_approximator, dummy_history)
 
-def test__train__valid():
+def test__train_1dspatial_temporal():
     DIFFUSIVITY, X_MIN, X_MAX, T_MIN, T_MAX = 0.3, 0.0, 2.0, 0.0, 3.0
 
     def heat_equation_1d(u, x, t):
@@ -237,10 +237,61 @@ def test__train__valid():
         return torch.mean((uu - (xx+tt))**2)
     metrics = {'dummy_mse': dummy_mse}
 
-    train_epoch_loss, train_epoch_metrics = _train(s_gen, t_gen, fcnn_approximator, adam, metrics, shuffle=True, batch_size=100)
+    train_epoch_loss, train_epoch_metrics = _train_1dspatial_temporal(s_gen, t_gen, fcnn_approximator, adam, metrics, shuffle=True, batch_size=100)
     assert train_epoch_loss > 0
     assert train_epoch_metrics['dummy_mse'] > 0
 
-    valid_epoch_loss, valid_epoch_metrics = _valid(s_gen, t_gen, fcnn_approximator, metrics)
+
+def test__valid_1dspatial_temporal():
+    DIFFUSIVITY, X_MIN, X_MAX, T_MIN, T_MAX = 0.3, 0.0, 2.0, 0.0, 3.0
+
+    def heat_equation_1d(u, x, t):
+        return diff(u, t) - DIFFUSIVITY * diff(u, x, order=2)
+
+    initial_condition = FirstOrderInitialCondition(u0=lambda x: torch.sin(PI * x))
+
+    def points_gen_lo():
+        while True:
+            yield torch.tensor([X_MIN])
+
+    dirichlet_boundary_lo = BoundaryCondition(
+        form=lambda u, x, t: torch.zeros_like(u),
+        points_generator=points_gen_lo()
+    )
+
+    def points_gen_hi():
+        while True:
+            yield torch.tensor([X_MAX])
+
+    dirichlet_boundary_hi = BoundaryCondition(
+        form=lambda u, x, t: torch.zeros_like(u),
+        points_generator=points_gen_hi()
+    )
+
+    fcnn = FCNN(
+        n_input_units=2,
+        n_output_units=1,
+        n_hidden_units=32,
+        n_hidden_layers=1,
+        actv=nn.Tanh
+    )
+    fcnn_approximator = SingleNetworkApproximator1DSpatialTemporal(
+        single_network=fcnn,
+        pde=heat_equation_1d,
+        initial_condition=initial_condition,
+        boundary_conditions=[dirichlet_boundary_lo, dirichlet_boundary_hi]
+    )
+
+    s_gen = generator_1dspatial(size=32, x_min=X_MIN, x_max=X_MAX)
+    t_gen = generator_temporal(size=32, t_min=T_MIN, t_max=T_MAX)
+
+    adam = optim.Adam(fcnn_approximator.parameters())
+
+    def dummy_mse(uu, xx, tt):
+        return torch.mean((uu - (xx + tt)) ** 2)
+
+    metrics = {'dummy_mse': dummy_mse}
+
+    valid_epoch_loss, valid_epoch_metrics = _valid_1dspatial_temporal(s_gen, t_gen, fcnn_approximator, metrics)
     assert valid_epoch_loss > 0
     assert valid_epoch_metrics['dummy_mse'] > 0
