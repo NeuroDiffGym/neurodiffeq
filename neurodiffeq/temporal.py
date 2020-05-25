f40bd772f6671b74acdce4c73c058db1073a6487
Feiyu <feiyu_chen@g.harvard.edu>
2020-05-25 14:08:30 -0400

    more docstring typo

--- neurodiffeq/temporal.py
+++ neurodiffeq/temporal.py
@@ -332,17 +332,7 @@ class SecondOrderInitialCondition:
 class BoundaryCondition:
     """A boundary condition. It is used to initialize `temporal.Approximator`s.
 
-    :param form: The form of the boundary condition. For a 1D time-dependent problem, if the boundary condition demands that :math:`B(u, x) = 0`, then `form` should be a function that map 
-    :math:`u, x, t` to :math:`B(u, x)`. For a 2D steady-state problem, if the boundary condition
-    demands that :math:`B(u, x, y) = 0`, then `form` should be a function that map 
-    :math:`u, x, y` to :math:`B(u, x, y)`. For a 2D steady-state system, 
-    if the boundary condition demands that :math:`B(u_i, x, y) = 0`, then `form`
-    should be a function that map :math:`u_1, u_2, ..., u_n, x, y` to `B(u_i, x, y)`.
-    For 2D time-dependent problem, if the boundary condition demands that 
-    :math:`B(u, x, y) = 0`, then `form`
-    should be a function that map :math:`u, x, y, t` to `B(u_i, x, y)`. Basically the
-    function signature of `form` should be the same as the `pde` function of the
-        given `temporal.Approximator`.
+    :param form: The form of the boundary condition. For a 1D time-dependent problem, if the boundary condition demands that :math:`B(u, x) = 0`, then `form` should be a function that map :math:`u, x, t` to :math:`B(u, x)`. For a 2D steady-state problem, if the boundary condition demands that :math:`B(u, x, y) = 0`, then `form` should be a function that map :math:`u, x, y` to :math:`B(u, x, y)`. For a 2D steady-state system, if the boundary condition demands that :math:`B(u_i, x, y) = 0`, then `form` should be a function that map :math:`u_1, u_2, ..., u_n, x, y` to `B(u_i, x, y)`. For 2D time-dependent problem, if the boundary condition demands that :math:`B(u, x, y) = 0`, then `form` should be a function that map :math:`u, x, y, t` to `B(u_i, x, y)`. Basically the function signature of `form` should be the same as the `pde` function of the given `temporal.Approximator`.
     :type form: funcion
     :param points_generator: A generator that generate points on the boundary.
         It can be a `temporal.generator_1dspatial`, `temporal.generator_2dspatial_segment`,
