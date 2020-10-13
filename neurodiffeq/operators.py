578a876f6d412dace47cb1662be70e97823904d5
Shuheng Liu <wish1104@outlook.com>
2020-10-13 15:56:44 +0800

    docs(operator): add docs for spherical operators

--- neurodiffeq/operators.py
+++ neurodiffeq/operators.py
@@ -4,6 +4,23 @@ from neurodiffeq import diff
 
 
 def spherical_curl(u_r, u_theta, u_phi, r, theta, phi):
+    r""" derives and evaluates the spherical curl of a spherical vector field :math:`u`
+    :param u_r: :math:`r`-component of the vector field u, must have shape (n_samples, 1)
+    :type u_r: `torch.Tensor`
+    :param u_theta: :math:`\theta`-component of the vector field u, must have shape (n_samples, 1)
+    :type u_theta: `torch.Tensor`
+    :param u_phi: :math:`\phi`-component of the vector field u, must have shape (n_samples, 1)
+    :type u_phi: `torch.Tensor`
+    :param r: a vector of :math:`r`-coordinate values, must have shape (n_samples, 1)
+    :type r: `torch.Tensor`
+    :param theta: a vector of :math:`theta`-coordinate values, must have shape (n_samples, 1)
+    :type theta: `torch.Tensor`
+    :param phi: a vector of :math:`phi`-coordinate values, must have shape (n_samples, 1)
+    :type phi: `torch.Tensor`
+    :return: :math:`r`, :math:`\theta`, and :math:`\phi` components of the curl, each with shape (n_samples, 1)
+    :rtype: tuple[`torch.Tensor`]
+    """
+
     d_r = lambda u: diff(u, r)
     d_theta = lambda u: diff(u, theta)
     d_phi = lambda u: diff(u, phi)
@@ -16,6 +33,18 @@ def spherical_curl(u_r, u_theta, u_phi, r, theta, phi):
 
 
 def spherical_grad(u, r, theta, phi):
+    r""" derives and evaluates the spherical gradient of a spherical scalar field :math:`u`
+    :param u: a scalar field u, must have shape (n_samples, 1)
+    :type u: `torch.Tensor`
+    :param r: a vector of :math:`r`-coordinate values, must have shape (n_samples, 1)
+    :type r: `torch.Tensor`
+    :param theta: a vector of :math:`theta`-coordinate values, must have shape (n_samples, 1)
+    :type theta: `torch.Tensor`
+    :param phi: a vector of :math:`phi`-coordinate values, must have shape (n_samples, 1)
+    :type phi: `torch.Tensor`
+    :return: :math:`r`, :math:`\theta`, and :math:`\phi` components of the gradient, each with shape (n_samples, 1)
+    :rtype: tuple[`torch.Tensor`]
+    """
     grad_r = diff(u, r)
     grad_theta = diff(u, theta) / r
     grad_phi = diff(u, phi) / (r * sin(theta))
@@ -23,6 +52,22 @@ def spherical_grad(u, r, theta, phi):
 
 
 def spherical_div(u_r, u_theta, u_phi, r, theta, phi):
+    r""" derives and evaluates the spherical divergence of a spherical vector field :math:`u`
+    :param u_r: :math:`r`-component of the vector field u, must have shape (n_samples, 1)
+    :type u_r: `torch.Tensor`
+    :param u_theta: :math:`\theta`-component of the vector field u, must have shape (n_samples, 1)
+    :type u_theta: `torch.Tensor`
+    :param u_phi: :math:`\phi`-component of the vector field u, must have shape (n_samples, 1)
+    :type u_phi: `torch.Tensor`
+    :param r: a vector of :math:`r`-coordinate values, must have shape (n_samples, 1)
+    :type r: `torch.Tensor`
+    :param theta: a vector of :math:`theta`-coordinate values, must have shape (n_samples, 1)
+    :type theta: `torch.Tensor`
+    :param phi: a vector of :math:`phi`-coordinate values, must have shape (n_samples, 1)
+    :type phi: `torch.Tensor`
+    :return: the divergence evaluated at :math:`(r, \theta, \phi)`, with shape (n_samples, 1)
+    :rtype: `torch.Tensor`
+    """
     div_r = diff(u_r * r ** 2, r) / r ** 2
     div_theta = diff(u_theta * sin(theta), theta) / (r * sin(theta))
     div_phi = diff(u_phi, phi) / (r * sin(theta))
@@ -30,6 +75,18 @@ def spherical_div(u_r, u_theta, u_phi, r, theta, phi):
 
 
 def spherical_laplacian(u, r, theta, phi):
+    r""" derives and evaluates the spherical laplacian of a spherical scalar field :math:`u`
+    :param u: a scalar field u, must have shape (n_samples, 1)
+    :type u: `torch.Tensor`
+    :param r: a vector of :math:`r`-coordinate values, must have shape (n_samples, 1)
+    :type r: `torch.Tensor`
+    :param theta: a vector of :math:`theta`-coordinate values, must have shape (n_samples, 1)
+    :type theta: `torch.Tensor`
+    :param phi: a vector of :math:`phi`-coordinate values, must have shape (n_samples, 1)
+    :type phi: `torch.Tensor`
+    :return: the laplacian evaluated at :math:`(r, \theta, \phi)`, with shape (n_samples, 1)
+    :rtype: `torch.Tensor`
+    """
     d_r = lambda u: diff(u, r)
     d_theta = lambda u: diff(u, theta)
     d_phi = lambda u: diff(u, phi)
@@ -41,6 +98,22 @@ def spherical_laplacian(u, r, theta, phi):
 
 
 def spherical_vector_laplacian(u_r, u_theta, u_phi, r, theta, phi):
+    r""" derives and evaluates the spherical laplacian of a spherical vector field :math:`u`
+    :param u_r: :math:`r`-component of the vector field u, must have shape (n_samples, 1)
+    :type u_r: `torch.Tensor`
+    :param u_theta: :math:`\theta`-component of the vector field u, must have shape (n_samples, 1)
+    :type u_theta: `torch.Tensor`
+    :param u_phi: :math:`\phi`-component of the vector field u, must have shape (n_samples, 1)
+    :type u_phi: `torch.Tensor`
+    :param r: a vector of :math:`r`-coordinate values, must have shape (n_samples, 1)
+    :type r: `torch.Tensor`
+    :param theta: a vector of :math:`theta`-coordinate values, must have shape (n_samples, 1)
+    :type theta: `torch.Tensor`
+    :param phi: a vector of :math:`phi`-coordinate values, must have shape (n_samples, 1)
+    :type phi: `torch.Tensor`
+    :return: the laplacian evaluated at :math:`(r, \theta, \phi)`, with shape (n_samples, 1)
+    :rtype: `torch.Tensor`
+    """
     d_theta = lambda u: diff(u, theta)
     d_phi = lambda u: diff(u, phi)
     scalar_lap = lambda u: spherical_laplacian(u, r, theta, phi)
