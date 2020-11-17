da428745f6491a183db7f0a5da73c3ac24e9e9db
Shuheng Liu <wish1104@icloud.com>
2020-11-17 19:17:42 +0800

    test(pde): remove convergence tests which take too long

--- tests/test_pde.py
+++ tests/test_pde.py
@@ -9,6 +9,7 @@ from neurodiffeq.pde import DirichletBVP2D, IBVP1D
 from neurodiffeq.pde import DirichletControlPoint, NeumannControlPoint, Point, CustomBoundaryCondition
 from neurodiffeq.generators import PredefinedGenerator, Generator2D
 from neurodiffeq.pde import solve2D, solve2D_system, Monitor2D, make_animation
+from neurodiffeq.pde import Solution
 
 from pytest import raises
 
@@ -37,7 +38,6 @@ def test_monitor():
         batch_size=64,
         monitor=Monitor2D(check_every=1, xy_min=(0, 0), xy_max=(1, 1))
     )
-    print('Monitor test passed.')
 
 
 def test_train_generator():
@@ -71,7 +71,6 @@ def test_train_generator():
 
     with raises(ValueError):
         train_gen = Generator2D((32, 32), (0, 0), (1, 1), method='magic')
-    print('Generator test passed.')
 
     valid_gen = Generator2D((32, 32), (0, 0), (1, 1), method='equally-spaced-noisy')
     train_gen = Generator2D((32, 32), (0, 0), (1, 1), method='equally-spaced')
@@ -98,148 +97,19 @@ def test_laplace():
     )
 
     net = FCNN(n_input_units=2, n_hidden_units=32, n_hidden_layers=1)
-    solution_neural_net_laplace, _ = solve2D(
+    solution_neural_net_laplace, loss_history = solve2D(
         pde=laplace, condition=bc, xy_min=(0, 0), xy_max=(1, 1),
-        net=net, max_epochs=300,
+        net=net, max_epochs=3,
         train_generator=Generator2D((32, 32), (0, 0), (1, 1), method='equally-spaced-noisy', xy_noise_std=(0.01, 0.01)),
         batch_size=64
     )
-    solution_analytical_laplace = lambda x, y: np.sin(np.pi * y) * np.sinh(np.pi * (1 - x)) / np.sinh(np.pi)
-
-    xs, ys = np.linspace(0, 1, 101), np.linspace(0, 1, 101)
-    xx, yy = np.meshgrid(xs, ys)
-    sol_net = solution_neural_net_laplace(xx, yy, as_type='np')
-    sol_ana = solution_analytical_laplace(xx, yy)
-    assert isclose(sol_net, sol_ana, atol=0.01).all()
-    print('Laplace test passed.')
-
-
-def test_heat():
-
-    k, L, T = 0.3, 2, 3
-    heat = lambda u, x, t: diff(u, t) - k * diff(u, x, order=2)
-
-    ibvp = IBVP1D(
-        x_min=0, x_min_val=lambda t: 0,
-        x_max=L, x_max_val=lambda t: 0,
-        t_min=0, t_min_val=lambda x: torch.sin(np.pi * x / L)
-    )
-    net = FCNN(n_input_units=2, n_hidden_units=32, n_hidden_layers=1)
-
-    def mse(u, x, y):
-        true_u = torch.sin(np.pi * y) * torch.sinh(np.pi * (1 - x)) / np.sinh(np.pi)
-        return torch.mean((u - true_u) ** 2)
-
-    solution_neural_net_heat, _ = solve2D(
-        pde=heat, condition=ibvp, xy_min=(0, 0), xy_max=(L, T),
-        net=net, max_epochs=300,
-        train_generator=Generator2D((32, 32), (0, 0), (L, T), method='equally-spaced-noisy'),
-        batch_size=64, metrics={'mse': mse}
-    )
-    solution_analytical_heat = lambda x, t: np.sin(np.pi * x / L) * np.exp(-k * np.pi ** 2 * t / L ** 2)
-
-    xs = np.linspace(0, L, 101)
-    ts = np.linspace(0, T, 101)
-    xx, tt = np.meshgrid(xs, ts)
-    make_animation(solution_neural_net_heat, xs, ts) # test animation
-    sol_ana = solution_analytical_heat(xx, tt)
-    sol_net = solution_neural_net_heat(xx, tt, as_type='np')
-    assert isclose(sol_net, sol_ana, atol=0.01).all()
-    print('Heat test passed.')
-
-
-def test_neumann_boundaries_1():
-
-    k, L, T = 0.3, 2, 3
-    heat = lambda u, x, t: diff(u, t) - k * diff(u, x, order=2)
-    solution_analytical_heat = lambda x, t: np.sin(np.pi * x / L) * np.exp(-k * np.pi ** 2 * t / L ** 2)
-
-    # Dirichlet on the left Neumann on the right
-    ibvp = IBVP1D(
-        x_min=0, x_min_val=lambda t: 0,
-        x_max=L, x_max_prime=lambda t: -np.pi / L * torch.exp(-k * np.pi ** 2 * t / L ** 2),
-        t_min=0, t_min_val=lambda x: torch.sin(np.pi * x / L)
-    )
-
-    net = FCNN(n_input_units=2, n_hidden_units=32, n_hidden_layers=1)
-
-    solution_neural_net_heat, _ = solve2D(
-        pde=heat, condition=ibvp, xy_min=(0, 0), xy_max=(L, T),
-        net=net, max_epochs=300,
-        train_generator=Generator2D((32, 32), (0, 0), (L, T), method='equally-spaced-noisy'),
-        batch_size=64
-    )
-
-    xs = np.linspace(0, L, 101)
-    ts = np.linspace(0, T, 101)
-    xx, tt = np.meshgrid(xs, ts)
-    make_animation(solution_neural_net_heat, xs, ts)  # test animation
-    sol_ana = solution_analytical_heat(xx, tt)
-    sol_net = solution_neural_net_heat(xx, tt, as_type='np')
-    assert isclose(sol_net, sol_ana, atol=0.1).all()
-    print('Dirichlet on the left Neumann on the right test passed.')
-
-def test_neumann_boundaries_2():
-
-    k, L, T = 0.3, 2, 3
-    heat = lambda u, x, t: diff(u, t) - k * diff(u, x, order=2)
-    solution_analytical_heat = lambda x, t: np.sin(np.pi * x / L) * np.exp(-k * np.pi ** 2 * t / L ** 2)
-
-    # Neumann on the left Dirichlet on the right
-    ibvp = IBVP1D(
-        x_min=0, x_min_prime=lambda t: np.pi / L * torch.exp(-k * np.pi ** 2 * t / L ** 2),
-        x_max=L, x_max_val=lambda t: 0,
-        t_min=0, t_min_val=lambda x: torch.sin(np.pi * x / L)
-    )
-
-    net = FCNN(n_input_units=2, n_hidden_units=32, n_hidden_layers=1)
-
-    solution_neural_net_heat, _ = solve2D(
-        pde=heat, condition=ibvp, xy_min=(0, 0), xy_max=(L, T),
-        net=net, max_epochs=300,
-        train_generator=Generator2D((32, 32), (0, 0), (L, T), method='equally-spaced-noisy'),
-        batch_size=64
-    )
-
-    xs = np.linspace(0, L, 101)
-    ts = np.linspace(0, T, 101)
-    xx, tt = np.meshgrid(xs, ts)
-    make_animation(solution_neural_net_heat, xs, ts)  # test animation
-    sol_ana = solution_analytical_heat(xx, tt)
-    sol_net = solution_neural_net_heat(xx, tt, as_type='np')
-    assert isclose(sol_net, sol_ana, atol=0.1).all()
-    print('Neumann on the left Dirichlet on the right test passed.')
-
-def test_neumann_boundaries_3():
-    k, L, T = 0.3, 2, 3
-    heat = lambda u, x, t: diff(u, t) - k * diff(u, x, order=2)
-    solution_analytical_heat = lambda x, t: np.sin(np.pi * x / L) * np.exp(-k * np.pi ** 2 * t / L ** 2)
-
-    # Neumann on both sides
-    ibvp = IBVP1D(
-        x_min=0, x_min_prime=lambda t: np.pi / L * torch.exp(-k * np.pi ** 2 * t / L ** 2),
-        x_max=L, x_max_prime=lambda t: -np.pi / L * torch.exp(-k * np.pi ** 2 * t / L ** 2),
-        t_min=0, t_min_val=lambda x: torch.sin(np.pi * x / L)
-    )
-
-    net = FCNN(n_input_units=2, n_hidden_units=32, n_hidden_layers=1)
-
-    solution_neural_net_heat, _ = solve2D(
-        pde=heat, condition=ibvp, xy_min=(0, 0), xy_max=(L, T),
-        net=net, max_epochs=300,
-        train_generator=Generator2D((32, 32), (0, 0), (L, T), method='equally-spaced-noisy'),
-        batch_size=64
-    )
-
-    xs = np.linspace(0, L, 101)
-    ts = np.linspace(0, T, 101)
-    xx, tt = np.meshgrid(xs, ts)
-    make_animation(solution_neural_net_heat, xs, ts)  # test animation
-    sol_ana = solution_analytical_heat(xx, tt)
-    sol_net = solution_neural_net_heat(xx, tt, as_type='np')
-    assert isclose(sol_net, sol_ana, atol=0.1).all()
-    print('Neumann on the left Dirichlet on the right test passed.')
-
+    assert isinstance(solution_neural_net_laplace, Solution)
+    assert isinstance(loss_history, dict)
+    keys = ['train_loss', 'valid_loss']
+    for key in keys:
+        assert key in loss_history
+        assert isinstance(loss_history[key], list)
+    assert len(loss_history[keys[0]]) == len(loss_history[keys[1]])
 
 # def test_pde_system():
 #     def _network_output_2input(net, xs, ys, ith_unit):
@@ -493,7 +363,7 @@ def test_arbitrary_boundary():
             return np.meshgrid(x, y)
 
     def to_np(tensor):
-        return tensor.detach().numpy()
+        return tensor.detach().cpu().numpy()
 
     xx_train, yy_train = get_grid(
         x_from_to=(-1, 1), y_from_to=(-1, 1),
