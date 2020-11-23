82a58aa4fe9197d976d3e60eb046804c867564c5
Shuheng Liu <wish1104@icloud.com>
2020-11-23 17:27:45 +0800

    meta(warning): turn on `DeprecationWarning`s by default

--- neurodiffeq/__init__.py
+++ neurodiffeq/__init__.py
@@ -1,5 +1,10 @@
 import torch
+import warnings
 from .neurodiffeq import diff
 from .utils import set_tensor_type as _set_tensor_type  # Don't export this function
 
+# Set default float type to 64 bits
 _set_tensor_type(float_bits=64)
+
+# Turn on deprecation warning by default
+warnings.simplefilter('always', DeprecationWarning)
