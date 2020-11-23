efd174e4d5609e144b1bf249ea58b865a6c7398c
Shuheng Liu <wish1104@icloud.com>
2020-11-23 17:48:27 +0800

    api(neurodiffeq): export submodule names to top level

--- neurodiffeq/__init__.py
+++ neurodiffeq/__init__.py
@@ -3,6 +3,17 @@ import warnings
 from .neurodiffeq import diff
 from .utils import set_tensor_type as _set_tensor_type  # Don't export this function
 
+from . import conditions
+from . import function_basis
+from . import generators
+from . import networks
+from . import neurodiffeq
+from . import operators
+from . import pde
+from . import ode
+from . import pde_spherical
+from . import temporal
+
 # Set default float type to 64 bits
 _set_tensor_type(float_bits=64)
 
