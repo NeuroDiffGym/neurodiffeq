cf00836729db838f5b99b8798d69c2401a9f06da
Joy Parikh <52308264+joyparikh@users.noreply.github.com>
2021-03-11 01:58:23 +0530

    Add files via upload

--- neurodiffeq/solvers_utils.py
+++ neurodiffeq/solvers_utils.py
@@ -1,14 +1,44 @@
+import dill
+import numpy as np
+from abc import ABC, abstractmethod
 
 class PretrainedSolver(ABC):
 
-    @classmethod
-    def load(cls, solver_name_or_path, additional_params=...):
-        print("Load.....")
-        # Instantiate solver
-        solver = cls(*model_args, **model_kwargs)
+    
+    def save(self,filename):
+	#save_keys = ['diff_eqs','metrics','global_epoch','nets','conditions','criterion','optimizer','generator'] #'criterion','optimizer','generator',
+        save_dict = {
+        "metrics": self.metrics_fn,
+        "criterion": self.criterion,
+        "conditions": self.conditions,
+        "global_epoch": self.global_epoch, #loss_history
+        "nets": self.nets,
+        "optimizer": self.optimizer,
+        "diff_eqs": self.diff_eqs,
+        "generator": self.generator
+        }
+	#print('this is in solver file')
+        with open(filename,'wb') as file:
+            dill.dump(save_dict,file)
+			
+    @classmethod		
+    def load(cls, path):
+        with open(path,'rb') as file:
+            load_dict = dill.load(file)	
+
+        t = load_dict['generator']['train'].get_examples()[0].detach().numpy()
+        t_min = np.round(min(t))
+        t_max = np.round(max(t))
+		
+        solver = cls(ode_system = load_dict['diff_eqs'],
+                     conditions = load_dict['conditions'],
+                     criterion = load_dict['criterion'],
+                     metrics = load_dict['metrics'],
+                     nets = load_dict['nets'],
+                     optimizer = load_dict['optimizer'],
+                     train_generator = load_dict['generator']['train'],
+                     valid_generator = load_dict['generator']['valid'],
+                     t_min = t_min,
+                     t_max = t_max)
+					 
         return solver
-    
-    
-    def save(self, solver_name_or_path, save_to_hub=False):
-	    # Save the solver here
-        print("Save.....")
