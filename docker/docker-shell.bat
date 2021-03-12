b6d00901dccbe2d422f773ff3a83b41bd6409222
Joy Parikh <f2016136@pilani.bits-pilani.ac.in>
2021-03-12 13:27:17 +0530

    docker windows fix

--- docker/docker-shell.bat
+++ docker/docker-shell.bat
@@ -1,3 +1,3 @@
-call ./env.dev.win
+call env.bat
 docker build -t %IMAGE_NAME% -f Dockerfile ..
-docker run --rm --name %IMAGE_NAME% -ti --mount type=bind,source="%BASE_DIR%",target=/app %IMAGE_NAME%
+docker run --rm --name %IMAGE_NAME% -ti --mount type=bind,source= %BASE_DIR%,target=/app %IMAGE_NAME%
