82c34c421b30b9015b2c4399829dc63102d2e73a
Joy Parikh <f2016136@pilani.bits-pilani.ac.in>
2021-03-17 23:32:50 +0530

    fix(docker): windows jupyter within docker

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,10 +5,10 @@ The following commands are available:
     notebook
         Runs the jupyter notebook from http://127.0.0.1:9898/
 
-\033[0m
-"
-notebook(){
+\033[0m"
+notebook (){
     jupyter notebook --ip 0.0.0.0 --port 9898 --no-browser --allow-root
 }
+
 export -f notebook
 pipenv -v shell
