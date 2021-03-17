de7ee7d4358c051a25d3a275167089098cd20fbe
Shivas Jayaram <shivasj@gmail.com>
2021-03-17 11:35:36 -0400

    refactor(docker): Added jupyter startup command

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,2 +1,14 @@
 echo "Container is running!!!"
+echo -en "\033[92m
+The following commands are available:
+
+    notebook
+        Runs the jupyter notebook from http://127.0.0.1:8888/
+
+\033[0m
+"
+notebook(){
+    jupyter notebook --ip 0.0.0.0 --no-browser --allow-root
+}
+export -f notebook
 pipenv -v shell
