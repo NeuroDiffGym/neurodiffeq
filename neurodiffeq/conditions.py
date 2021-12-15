92eebe3fc88f94c264e407baecaa894a3d8ffd84
Elaine <mcunha@g.harvard.edu>
2021-12-14 16:38:04 -0800

    added condition for the wave eq

--- neurodiffeq/conditions.py
+++ neurodiffeq/conditions.py
@@ -409,6 +409,93 @@ class DirichletBVP2D(BaseCondition):
 
         return Axy + x_tilde * (1 - x_tilde) * y_tilde * (1 - y_tilde) * output_tensor
 
+class DirichletIBVP1D_test(BaseCondition):
+    r"""EC ADDITION: FOR THE WAVE EQ, OUT OF DATE COMMENTS
+    An Dirichlet boundary condition on the boundary of :math:`[x_0, x_1] \times [y_0, y_1]`, where
+
+    - :math:`u(x_0, y) = f_0(y)`;
+    - :math:`u(x_1, y) = f_1(y)`;
+    - :math:`u(x, y_0) = g_0(x)`;
+    - :math:`u(x, y_1) = g_1(x)`.
+
+    :param x_min: The lower bound of x, the :math:`x_0`.
+    :type x_min: float
+    :param x_min_val: The boundary value on :math:`x = x_0`, i.e. :math:`f_0(y)`.
+    :type x_min_val: callable
+    :param x_max: The upper bound of x, the :math:`x_1`.
+    :type x_max: float
+    :param x_max_val: The boundary value on :math:`x = x_1`, i.e. :math:`f_1(y)`.
+    :type x_max_val: callable
+    :param y_min: The lower bound of y, the :math:`y_0`.
+    :type y_min: float
+    :param y_min_val: The boundary value on :math:`y = y_0`, i.e. :math:`g_0(x)`.
+    :type y_min_val: callable
+    :param y_max: The upper bound of y, the :math:`y_1`.
+    :type y_max: float
+    :param y_max_val: The boundary value on :math:`y = y_1`, i.e. :math:`g_1(x)`.
+    :type y_max_val: callable
+    """
+
+    def __init__(self, x_min, x_min_val, x_max, x_max_val, t_min, t_min_val, t_min_prime, t_max):
+        r"""Initializer method
+        """
+        super().__init__()
+        self.x0, self.f0 = x_min, x_min_val
+        self.x1, self.f1 = x_max, x_max_val
+        self.t0, self.g0, self.gprime = t_min, t_min_val, t_min_prime
+        self.t1 = t_max
+
+    def parameterize(self, output_tensor, x, t):
+        r"""EC ADDITION: FOR THE WAVE EQ, OUT OF DATE COMMENTS
+        Re-parameterizes outputs such that the Dirichlet condition is satisfied on all four sides of the domain.
+
+        The re-parameterization is
+        :math:`\displaystyle u(x,y)=A(x,y)
+        +\tilde{x}\big(1-\tilde{x}\big)\tilde{y}\big(1-\tilde{y}\big)\mathrm{ANN}(x,y)`, where
+
+        :math:`\displaystyle \begin{align*}
+        A(x,y)=&\big(1-\tilde{x}\big)f_0(y)+\tilde{x}f_1(y) \\
+        &+\big(1-\tilde{y}\big)\Big(g_0(x)-\big(1-\tilde{x}\big)g_0(x_0)+\tilde{x}g_0(x_1)\Big) \\
+        &+\tilde{y}\Big(g_1(x)-\big(1-\tilde{x}\big)g_1(x_0)+\tilde{x}g_1(x_1)\Big)
+        \end{align*}`
+
+        :math:`\displaystyle\tilde{x}=\frac{x-x_0}{x_1-x_0}`,
+
+        :math:`\displaystyle\tilde{y}=\frac{y-y_0}{y_1-y_0}`,
+
+        and :math:`\mathrm{ANN}` is the neural network.
+
+        :param output_tensor: Output of the neural network.
+        :type output_tensor: `torch.Tensor`
+        :param x: :math:`x`-coordinates of inputs to the neural network; i.e., the sampled :math:`x`-coordinates.
+        :type x: `torch.Tensor`
+        :param y: :math:`y`-coordinates of inputs to the neural network; i.e., the sampled :math:`y`-coordinates.
+        :type y: `torch.Tensor`
+        :return: The re-parameterized output of the network.
+        :rtype: `torch.Tensor`
+        """
+        x_tilde = (x - self.x0) / (self.x1 - self.x0)
+        t_tilde = (t - self.t0) / (self.t1 - self.t0)
+        Axt = torch.sin(np.pi * x)
+        u_adj = Axt + x_tilde * (1 - x_tilde) * (1 - torch.exp(-t_tilde ** 2)) * output_tensor
+        
+#         x_tilde = (x-self.xmin) / (self.xmax-self.xmin)
+#         t_tilde = (t-self.tmin) / (self.tmax-self.tmin)
+#         Axt = torch.sin(self.pi * x)
+#         u_adj = Axt + x_tilde*(1-x_tilde)*(1 - torch.exp(-t_tilde**2))*u
+
+#         x_tilde = (x - self.x0) / (self.x1 - self.x0)
+#         y_tilde = (y - self.y0) / (self.y1 - self.y0)
+#         x0 = torch.ones_like(x_tilde[0, 0]).expand(*x_tilde.shape) * self.x0
+#         x1 = torch.ones_like(x_tilde[0, 0]).expand(*x_tilde.shape) * self.x1
+#         Axy = (1 - x_tilde) * self.f0(y) + x_tilde * self.f1(y) \
+#               + (1 - y_tilde) * (self.g0(x) - ((1 - x_tilde) * self.g0(x0) + x_tilde * self.g0(x1))) \
+#               + y_tilde * (self.g1(x) - ((1 - x_tilde) * self.g1(x0) + x_tilde * self.g1(x1)))
+
+#         return Axy + x_tilde * (1 - x_tilde) * y_tilde * (1 - y_tilde) * output_tensor
+
+        return u_adj
+
 
 class IBVP1D(BaseCondition):
     r"""An initial & boundary condition on a 1-D range where :math:`x\in[x_0, x_1]` and time starts at :math:`t_0`.
